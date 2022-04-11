1183f987485ec81191ea1f0be80bb63d7d59bd96
José Nildo de Macedo Pereira <jnmpdev@gmail.com>
2022-04-10 21:22:56 -0400

    Start: Create the "SalesTax_SwiftUI" project
    Created a new project called "SalesTax_SwiftUI", to accompany the course "iOS App Development: The Big Picture", by Pluralsight, it will be basically the same "SalesTax_Storyboard", but will use SwiftUI instead of Storyboard

---
+++ 01 - iOS App Development - The Big Picture/SalesTax_SwiftUI/SalesTax_SwiftUI/SalesTax_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesTax_SwiftUIApp.swift
+//  SalesTax_SwiftUI
+//
+//  Created by user207017 on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SalesTax_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
