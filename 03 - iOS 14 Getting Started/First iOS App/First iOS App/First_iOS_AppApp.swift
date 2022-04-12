bd13ef597169e44e63d6f94951ac62cf54009977
José Nildo de Macedo Pereira <jnmpdev@gmail.com>
2022-04-11 22:05:06 -0300

    Start: Create the "First iOS App" project
    Created a new project called "First iOS App", to accompany the course "iOS 14 Getting Started", by Pluralsight, it used SwiftUI

---
+++ 03 - iOS 14 Getting Started/First iOS App/First iOS App/First_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_iOS_AppApp.swift
+//  First iOS App
+//
+//  Created by Jose Nildo de Macedo Pereira on 11/04/22.
+//
+
+import SwiftUI
+
+@main
+struct First_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
