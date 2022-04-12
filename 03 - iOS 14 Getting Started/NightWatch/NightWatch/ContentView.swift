376fd5fccc9ca248596c12be9be6b66e677b9a83
José Nildo de Macedo Pereira <jnmpdev@gmail.com>
2022-04-11 22:42:33 -0300

    Start: Create the "NightWatch" project
    Created a new project called "NightWatch" to accompany the course "iOS 14 Getting Started", by Pluralsight, it will use SwiftUI and will be a to-do list application

---
+++ 03 - iOS 14 Getting Started/NightWatch/NightWatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NightWatch
+//
+//  Created by Jose Nildo de Macedo Pereira on 11/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 03 - iOS 14 Getting Started/NightWatch/NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Jose Nildo de Macedo Pereira on 11/04/22.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
