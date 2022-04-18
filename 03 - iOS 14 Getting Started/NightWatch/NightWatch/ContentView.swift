c09151cc88775f8e2a94497be25f90033e120b1b
José Nildo de Macedo Pereira <jnmpdev@gmail.com>
2022-04-18 13:31:16 -0400

    Feat: Make Task Screen
    Finished task screen

--- 03 - iOS 14 Getting Started/NightWatch/NightWatch/ContentView.swift
+++ 03 - iOS 14 Getting Started/NightWatch/NightWatch/ContentView.swift
@@ -9,8 +9,75 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Group {
+                        HStack {
+                            Text(Image(systemName: "moon.stars"))
+                            .foregroundColor(.yellow)
+                            .font(.title3)
+                            .fontWeight(.heavy)
+                            Text("Nightly Tasks")
+                            .underline()
+                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.yellow)
+                        }
+                        Text("- Check all windows")
+                        Text("- Check all doors")
+                        Text("- Check that the afe is locked")
+                        Text("- Check the mailbox")
+                        Text("- Inspect security cameras")
+                        Text("- Clear ice from sidewalks")
+                        Text("- Document \"strange and unsual\" occurrences")
+                    }
+                    
+                    Group {
+                        Divider()
+                        HStack {
+                            Text(Image(systemName: "sunset"))
+                            .foregroundColor(.yellow)
+                            .font(.title3)
+                            .fontWeight(.heavy)
+                            Text("Weekly Tasks")
+                            .underline()
+                            .font(.title3)
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.yellow)
+                            .padding(.top)
+                        }
+                        
+                        Text("- Check inside all vacant rooms")
+                        Text("- Walk the perimeter of property")
+                    }
+                    
+                    Group {
+                        Divider()
+                        HStack {
+                            Text(Image(systemName: "calendar"))
+                            .foregroundColor(.yellow)
+                            .font(.title3)
+                            .fontWeight(.heavy)
+                            Text("Monthly Tasks")
+                            .underline()
+                            .font(.title3)
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.yellow)
+                            .padding(.top)
+                        }
+                        
+                        Text("- Test security alarm")
+                        Text("- Test motion detectors")
+                        Text("- Test smoke alarms")
+                    }
+                }
+                .foregroundColor(.gray)
+                Spacer()
+            }
+            .padding([.top, .leading])
+            Spacer()
+        }
     }
 }
 
